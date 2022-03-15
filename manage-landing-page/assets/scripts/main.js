19cc85855c875f1121d227a90a484364ad0e03f3
alialaba <alialaba079@gmail.com>
2022-03-14 17:15:41 -0700

    style button and responsive navigation

--- manage-landing-page/assets/scripts/main.js
+++ manage-landing-page/assets/scripts/main.js
@@ -9,3 +9,40 @@ const toggleNav = () => {
 //EVENTLISTENER
 menuBtnIcon.addEventListener("click", toggleNav)
 
+
+// slideshow functionality
+
+// let slideIndex = 1;
+
+// const showSlides = (n) => {
+//     let i;
+//     let slides = document.getElementsByClassName("testimonial__item");
+//     let dots = document.getElementsByClassName("dots__circle")
+
+//     //conditional checking
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+
+//     //for loop thru slides
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+
+//     //for loop dots
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+
+
+//     console.log(slides, dots)
+// }
+// showSlides(slideIndex)
+
+// const plusSlide = (n) => {
+//     showSlides(slideIndex += n)
+// }
+// const currentSlide = (n) => {
+//     showSlides(slideIndex = n)
+// }
