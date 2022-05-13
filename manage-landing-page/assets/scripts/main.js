e9341c4e790052d8171798bc4f462190df7368b8
alialaba <alialaba079@gmail.com>
2022-05-12 17:33:22 -0700

    remove overlay functionality

--- manage-landing-page/assets/scripts/main.js
+++ manage-landing-page/assets/scripts/main.js
@@ -1,24 +1,15 @@
 //VARIABLES
 const headerEl = document.querySelector(".header");
 const menuBtnIcon = document.querySelector(".menu__icon");
-const overlay = document.getElementById("overlay");
+
 
 //TOGGLE FUNCTION
 const toggleNav = () => {
     headerEl.classList.toggle("nav--open");
-    overlay.classList.toggle("show");
+
 
 }
 //EVENTLISTENER
 menuBtnIcon.addEventListener("click", toggleNav)
-
-// menuBtnIcon.addEventListener("click", (event) => {
-//     event.stopPropagation()
-//     overlay.classList.add("show");
-// })
-// menuBtnIcon.addEventListener("click", (event) => {
-//     event.stopPropagation()
-//     overlay.classList.remove("show");
-// })
 
 
